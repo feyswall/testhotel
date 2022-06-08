9944842206e91f0fea2f10c3ab73642e6e5d53a7
feyswall <vicentmkota91@gmail.com>
2022-06-08 15:31:52 +0300

    last

--- resources/views/pageLayouts/admin.blade.php
+++ resources/views/pageLayouts/admin.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-	@include('_partials._head')
+	@include('admin._partials._head')
 </head>
 <!--
   HOW TO USE: 
@@ -15,11 +15,11 @@
 <body data-theme="default" data-layout="fluid" data-sidebar-position="left" data-sidebar-layout="default">
 	<div class="wrapper">
 		<!-- sidebar -->
-		@include('_partials._sideBar')
+		@include('admin._partials._sideBar')
 
 		<div class="main">
 		<!-- top navigation -->
-		@include('_partials._topNavigation')
+		@include('admin._partials._topNavigation')
 
 
 			@yield('content')
@@ -28,12 +28,12 @@
 			
 
 			{{-- footer --}}
-			@include('_partials._footer')
+			@include('admin._partials._footer')
 		</div>
 	</div>
 
 	<!-- down script -->
-	@include('_partials._downScript')
+	@include('admin._partials._downScript')
 
 	@yield('ourScript')
 </body>
