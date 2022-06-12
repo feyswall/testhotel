0296008343b8d463c6f56b17b070e762db082491
shughulijr07 <abdumalikiandrew@gmail.com>
2022-06-12 12:33:18 +0300

    upd

--- resources/views/admin/_partials/_sideBar.blade.php
+++ resources/views/admin/_partials/_sideBar.blade.php
@@ -1,27 +1,15 @@
 <nav id="sidebar" class="sidebar js-sidebar">
-    <div class="sidebar-content js-simplebar">
-        <a class="sidebar-brand" href="index.html">
-            <span class="sidebar-brand-text align-middle">
-                Dashboard
-            </span>
-            <svg class="sidebar-brand-icon align-middle" width="32px" height="32px" viewbox="0 0 24 24" fill="none"
-                stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="miter" color="#FFFFFF"
-                style="margin-left: -3px">
-                <path d="M12 4L20 8.00004L12 12L4 8.00004L12 4Z"></path>
-                <path d="M20 12L12 16L4 12"></path>
-                <path d="M20 16L12 20L4 16"></path>
-            </svg>
+    <div class="sidebar-content js-simplebar logo-dark">
+        <a class="sidebar-brand" href="/dashboard">
+            <img src="{{ asset('assets/img/avatars/logo-dark.png') }}" alt="" width="100%" height="100%">
         </a>
 
-        <div class="sidebar-user">
-            <div class="d-flex justify-content-center">
-            </div>
-        </div>
+        <hr>
 
         <ul class="sidebar-nav">
-            <li class="sidebar-header">
+            {{-- <li class="sidebar-header">
                 Pages
-            </li>
+            </li> --}}
             <li class="sidebar-item">
 
             <li class="sidebar-item">
