b0c0aa93af3cabad7b27879243de4186f071d4d5
shughulijr07 <abdumalikiandrew@gmail.com>
2022-06-16 20:43:45 +0300

    excel sheet

--- app/Imports/ItemsImport.php
+++ app/Imports/ItemsImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Models\Item;
+use Exception;
 use Maatwebsite\Excel\Concerns\ToModel;
 
 
@@ -22,22 +23,34 @@ class ItemsImport implements ToModel, WithValidation
 
 
     public function model(array $row)
-    {
-        return new Item([
-            'name' => $row[2],
-            'code' => $row[2],
-            'desc' => $row[4] ?? null,
-            'pref_supplier' => $row[6] ?? null,
-            'selling_price' => $row[8],
-            'gross_price' => $row[10],
-        ]);
+    {        
+        foreach($row as $item){
+            if($item != null){
+                if($row[2] == null){
+                    continue;
+                }
+                try{
+                    return new Item([
+                        'name' => $row[2],
+                        'code' => $row[2],
+                        'desc' => $row[4] ?? null,
+                        'pref_supplier' => $row[6] ?? null,
+                        'selling_price' => $row[8] ?? 0,
+                        'gross_price' => $row[10] ?? 0,
+                    ]);
+                }catch(Exception $e){
+                    error_log("Got an exception..".$e->getMessage());
+                }
+            }
+        }
+
     }
 
 
      public function rules(): array
     {
         return [
-            '*.2' => 'exclude_if:*.8,null:*.10,null|required',
+            // '*.2' => 'exclude_if:*.8,null:*.10,null|required',
         ];
     }
 
