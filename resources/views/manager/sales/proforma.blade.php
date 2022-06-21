da5797ac9aee536a1b1b3e7e36a0ba6906a359e3
feyswall <vicentmkota91@gmail.com>
2022-06-21 18:12:12 +0300

    prof file

--- resources/views/manager/sales/proforma.blade.php
+++ resources/views/manager/sales/proforma.blade.php
@@ -154,7 +154,7 @@
                                     <td colspan="2" class="text-end">{{number_format($purchase->subtotal, 2)}}</td>
                                 </tr>
                                 <tr>
-                                    <td colspan="5" class="font-weight-bold text-secondary">VAT Total ({{$vat_rate}}%)</td>
+                                    <td colspan="5" class="font-weight-bold text-secondary">VAT Total ({{$vat->rate}}%)</td>
                                     <td colspan="2" class="text-end">{{number_format($purchase->vat_total, 2)}}</td>
                                 </tr>
                                 <tr>
