6185376f87312cbefbe0bd9f4c9d9790741df529
shughulijr07 <abdumalikiandrew@gmail.com>
2022-06-21 15:45:39 +0300

    ui updates

--- resources/views/manager/items/edit.blade.php
+++ resources/views/manager/items/edit.blade.php
@@ -5,12 +5,12 @@
 @endsection
 
 @section('content')
-     <main class="content">
+     <main class="content p-4">
         <div class="container-fluid p-0">
             <a href="/items" class="btn btn-primary float-end mt-n1">View Items</a>
 
             <div class="mb-3">
-                <h1 class="h3 d-inline align-middle"> New Item</h1>
+                <h1 class="h3 d-inline align-middle"> Edit Item</h1>
             </div>
 
 
