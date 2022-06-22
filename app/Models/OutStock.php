d4eccc5105b86bffc6be4278de84bcdc9744ca5f
feyswall <vicentmkota91@gmail.com>
2022-06-22 14:27:40 +0300

    stock finished

---
+++ app/Models/OutStock.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OutStock extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['in_stock_id', 'quantity', 'date_out',
+         'stock_mode_in', 'out_to'];
+}
---
+++ database/migrations/2022_06_22_112455_create_out_stocks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('out_stocks', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('in_stock_id');
+
+            $table->double('quantity');
+            $table->dateTime('date_out');
+
+            $table->foreignId('stock_mode_id')->constrained('stock_modes');
+
+            $table->integer('out_to');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('out_stocks');
+    }
+};
